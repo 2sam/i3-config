e5de0e3dd0eed6f44c2d9e1177097b5da10105c6
2sam <2sam@phantasie.cc>
2017-08-08 02:14:39 +0200

    Update lock-start.sh

--- lock-start.sh
+++ lock-start.sh
@@ -4,27 +4,7 @@ start() {
   killall xss-lock
   #echo "starting xss-lock"
   xss-lock -- i3lock -i ~/docs/lock.png -tfd &
-  watch $!
-}
-
-watch() {
-  PID=$1
-
-  while ps $PID; do
-    #echo "watching wifi"
-    if iwconfig wlp3s0|grep 'Grassenberg e.V. OBEN'; then 
-      kill $PID
-      #echo "killed xss-lock"
-    else sleep 3s; fi
-  done
-
-  while iwconfig wlp3s0|grep OBEN; do 
-    #echo "watching wifi"  
-    sleep 3s; 
-  done
-
-  #echo "starting xss-lock"
-  start
+  #watch $!
 }
 
 start
